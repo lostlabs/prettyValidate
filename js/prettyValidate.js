ad2b872c964dc01e97b13f904b55280011839edf
Mason Miller <masonjmiller@hotmail.com>
2016-03-23 15:39:41 -0400

    Optimized Method for Validation

--- js/prettyValidate.js
+++ js/prettyValidate.js
@@ -5,63 +5,31 @@
  * details, see https://creativecommons.org/licenses/by/3.0/.
  */
 
-function validateText(text) {
-	var pattern = /^[a-zA-Z ]*$/;
-	return pattern.test(text);
-}
-
-function validateNoHTML(text) {
-	var pattern = /^(?!.*<[^>]+>).*/;
-	return pattern.test(text);
-}
-
-function validateNumber(text) {
-	var pattern = /^[0-9]*$/;
-	return pattern.test(text);
-}
-
-function validateEmail(email) {
-	var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-	return pattern.test(email);
-}
-
-function validatePhone(phone) {
-	var pattern = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
-	return pattern.test(phone);
-}
-
-function validateColor(color) {
-	var pattern = /^#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$/;
-	return pattern.test(color);
-}
-
-function validateDate(date) {
-	var pattern = /^((0?[13578]|10|12)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[01]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1}))|(0?[2469]|11)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[0]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1})))$/;
-	return pattern.test(date);
-}
-
-function valid(input) {
-	input.removeClass("form-control-danger form-control-warning");
-	input.addClass("form-control-success");
-	input.parent().removeClass("has-danger has-warning");
-	input.parent().addClass("has-success");
-}
-
-function invalid(input) {
-	input.removeClass("form-control-danger form-control-success");
-	input.addClass("form-control-warning");
-	input.parent().removeClass("has-danger has-success");
-	input.parent().addClass("has-warning");
-}
-
-function empty(input) {
-	input.removeClass("form-control-warning form-control-success");
-	input.addClass("form-control-danger");
-	input.parent().removeClass("has-warning has-success");
-	input.parent().addClass("has-danger");
-}
 
 (function($) {
+	
+	function validate(input, type) {
+		var pattern = {
+				text: /^[a-zA-Z ]*$/,
+				noHTML: /^(?!.*<[^>]+>).*/,
+				number: /^[0-9]*$/,
+				email: /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i,
+				phone: /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/,
+				hex: /^#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$/,
+				date: /^((0?[13578]|10|12)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[01]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1}))|(0?[2469]|11)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[0]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1})))$/
+			},
+			valid = function(input) { input.removeClass("form-control-danger form-control-warning").addClass("form-control-success").parent().removeClass("has-danger has-warning").addClass("has-success"); },
+			invalid = function(input) { input.removeClass("form-control-danger form-control-success").addClass("form-control-warning").parent().removeClass("has-danger has-success").addClass("has-warning"); },
+			empty = function(input) { input.removeClass("form-control-warning form-control-success").addClass("form-control-danger").parent().removeClass("has-warning has-success").addClass("has-danger"); },
+			error = 0;
+		$.each(pattern, function(i, p){
+			if (i == type) {
+				if (!input.val()) { empty(input); error++; }
+				else if (!p.test(input.val())) { invalid(input); error++; }
+				else { valid(input); }
+			}
+		}); if (error != 0) return true;
+	}
 	
 	$.fn.prettyValidate = function(options) {
 	
@@ -87,8 +55,7 @@ function empty(input) {
 			// Load Color Field
 			$('.form-control[type="color"]', this).each(function() {
 				$('<label for="' + $(this).attr("name") + '"><i class="fa fa-paint-brush"></i></label>').insertAfter(this);
-				$(this).addClass('color').attr('type','text').colourBrightness();
-				$(this).colorpicker({
+				$(this).addClass('color').attr('type','text').colourBrightness().colorpicker({
 					align: 'left',
 					container: $(this).parent(),
 					format: 'hex'
@@ -103,7 +70,7 @@ function empty(input) {
 				var val = $(this).val();
 				var min = ($(this).attr('min')) ? $(this).attr('min') : 0;
 				var max = ($(this).attr('max')) ? $(this).attr('max') : 100;
-				if ($(this).attr("placeholder")) {
+				if ($(this).attr("placeholder") && !$(this).prev('label').length) {
 					$('<label for="'+ $(this).attr('name') +'">'+ $(this).attr("placeholder") +':</label>').insertBefore(this);
 					$('<label class="value">'+ val +'</label>').insertBefore(this);
 				}
@@ -112,20 +79,16 @@ function empty(input) {
 				var minWidth = $(this).parent().find("label.min").outerWidth();
 				var maxWidth = $(this).parent().find("label.max").outerWidth();
 				var width = minWidth + maxWidth + 20;
-				$(this).css({width: "calc(100% - "+ width +"px)", marginLeft: minWidth + 10});
-				$(this).on('input', function() {
+				$(this).css({width: "calc(100% - "+ width +"px)", marginLeft: minWidth + 10}).on('input', function() {
 					var val = $(this).val();
-					if ($(this).attr("placeholder")) {
-						$(this).parent().find("label.value").html(val);
-					}
+					if ($(this).attr("placeholder")) { $(this).parent().find("label.value").html(val); }
 				});
 			});
 			
 			// Load Date Field
 			$('.form-control[type="date"]', this).each(function() {
 				$('<label for="' + $(this).attr("name") + '"><i class="fa fa-calendar"></i></label>').insertAfter(this);
-				$(this).addClass('date').attr('type','text');
-				$(this).datepicker({
+				$(this).addClass('date').attr('type', 'text').datepicker({
 					autoclose: true,
 					clearBtn: true,
 					container: $(this).parent(),
@@ -141,23 +104,17 @@ function empty(input) {
 				var input = $('fieldset input, fieldset textarea', form);
 				input.each(function() {
 					if ($(this).hasClass('required') || this.value) {
-						if (!this.value) { empty($(this)); error++; }
-						else if (
-							!$(this).hasClass("date") &&
-							!$(this).hasClass("color") &&
-							$(this).attr("type") == "text" &&
-							!validateText($(this).val())
-						) { invalid($(this)); error++; }
-						else if ($(this).attr("type") == "number" && !validateNumber($(this).val())) { invalid($(this)); error++; }
-						else if ($(this).attr("type") == "email" && !validateEmail($(this).val())) { invalid($(this)); error++; }
-						else if ($(this).attr("type") == "phone" && !validatePhone($(this).val())) { invalid($(this)); error++; }
-						else if ($(this).attr("type") == "range") {}
-						else if ($(this).hasClass("color") && !validateColor($(this).val())) { invalid($(this)); error++; }
-						else if ($(this).hasClass("date") && !validateDate($(this).val())) { invalid($(this)); error++; }
-						else if ($(this).is("textarea") && !validateNoHTML($(this).val())) { invalid($(this)); error++; }
-						else { valid($(this)); }
+						if ($(this).attr("type") == "text" && !$(this).is(".date, .color") && validate($(this), 'text')) { error++; }
+						if ($(this).attr("type") == "number" && validate($(this), 'number')) { error++; }
+						if ($(this).attr("type") == "email" && validate($(this), 'email')) { error++; }
+						if ($(this).attr("type") == "phone" && validate($(this), 'phone')) { error++; }
+						if ($(this).attr("type") == "range") {}
+						if ($(this).hasClass("color") && validate($(this), 'hex')) { error++; }
+						if ($(this).hasClass("date") && validate($(this), 'date')) { error++; }
+						if ($(this).is("textarea") && validate($(this), 'noHTML')) { error++; }
 					}
 				});
+				console.log(error);
 				if (error == 0) {
 					$.isFunction(options.valid) && options.valid.call(this);
 					if (!$.isFunction(options.valid) && !options.ajax) { return true }
@@ -167,28 +124,20 @@ function empty(input) {
 							data: form.serialize(),
 							dataType: "json",
 							type: "post",
-							success: function(data) {
-								options.success.call(this, data)
-							},
-							error: function(data) {
-								options.error.call(this, data)
-							},
-							complete: function(data) {
-								options.complete.call(this, data)
-							}
+							success: function(data) { options.success.call(this, data) },
+							error: function(data) { options.error.call(this, data) },
+							complete: function(data) { options.complete.call(this, data) }
 						});
-						return false
+						return false;
 					}
 					if (options.reset) form[0].reset();
 				} else {
 					$.isFunction(options.invalid) && options.invalid.call(this);
 					if (options.shake) {
 						$(".has-danger, .has-warning", form).addClass("shake");
-						setTimeout(function() {
-							$(".has-danger, .has-warning", form).removeClass("shake");
-						}, 500);
+						setTimeout(function() { $(".has-danger, .has-warning", form).removeClass("shake"); }, 500);
 					}
-					return false
+					return false;
 				}
 			});
 			
@@ -204,24 +153,17 @@ function empty(input) {
 				$("input, textarea", this).each(function() { $(this).removeClass("form-control-danger form-control-warning form-control-success"); });
 			});
 			
-			// On Input Change
-			$("input, textarea", this).on('change keyup blur input', function() {
+			// Detect Input Changes
+			$("input, textarea", this).on('change changeColor changeDate input keyup blur', function() {
 				if ($(this).hasClass('required') || this.value) {
-					if (!this.value) { empty($(this)); }
-					else if (
-						!$(this).hasClass("date") &&
-						!$(this).hasClass("color") &&
-						$(this).attr("type") == "text" &&
-						!validateText($(this).val())
-					) { invalid($(this)); }
-					else if ($(this).attr("type") == "number" && !validateNumber($(this).val())) { invalid($(this)); }
-					else if ($(this).attr("type") == "email" && !validateEmail($(this).val())) { invalid($(this)); }
-					else if ($(this).attr("type") == "phone" && !validatePhone($(this).val())) { invalid($(this)); }
-					else if ($(this).attr("type") == "range") {}
-					else if ($(this).hasClass("color") && !validateColor($(this).val())) { invalid($(this)); }
-					else if ($(this).hasClass("date") && !validateDate($(this).val())) { invalid($(this)); }
-					else if ($(this).is("textarea") && !validateNoHTML($(this).val())) { invalid($(this)); }
-					else { valid($(this)); }
+					if ($(this).attr("type") == "text" && !$(this).is(".date, .color")) { validate($(this), 'text'); }
+					if ($(this).attr("type") == "number") { validate($(this), 'number'); }
+					if ($(this).attr("type") == "email") { validate($(this), 'email'); }
+					if ($(this).attr("type") == "phone") { validate($(this), 'phone'); }
+					if ($(this).attr("type") == "range") {}
+					if ($(this).hasClass("color")) { validate($(this), 'hex'); }
+					if ($(this).hasClass("date")) { validate($(this), 'date'); }
+					if ($(this).is("textarea")) { validate($(this), 'noHTML'); }
 				} else {
 					$(this).removeClass("form-control-danger form-control-warning form-control-success");
 					$(this).parent().removeClass("has-danger has-warning has-success");
